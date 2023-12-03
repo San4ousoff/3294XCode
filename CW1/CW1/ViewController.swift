bf26443bf1b330feebfba15505d53abf92b50686
San4o <san4ousoff@mail.ru>
2023-12-03 19:37:42 +0200

    First commit form terminal

---
+++ CW1/CW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CW1
+//
+//  Created by Mac on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW1/HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Mac on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
